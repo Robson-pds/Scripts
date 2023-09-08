1aaa6fef0c7d46d71d65feb0bfaeb80d33fd8504
Robson Pereira Da Silva <43390496+Robson-pds@users.noreply.github.com>
2023-09-08 17:47:26 -0300

    Update ScriptUbuntuNodejs.sh
    Atualização do script para nova forma de instalação do nodejs, seguindo o github.

--- ScriptUbuntuNodejs.sh
+++ ScriptUbuntuNodejs.sh
@@ -3,7 +3,11 @@
 sudo apt update
 sudo apt upgrade  -y -f
 sudo apt dist-upgrade  -y -f
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y -f nodejs yarn
 sudo npm install -g pm2
