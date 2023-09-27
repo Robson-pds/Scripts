f29de3d38a6197a60e1f18b3acd9c08416db4b9d
Robson Pereira Da Silva <43390496+Robson-pds@users.noreply.github.com>
2023-09-27 18:11:41 -0300

    Update ScriptUbuntuBase.sh
    Alterado para zsh da pasta do usuario logado e não da pasta atual

--- ScriptUbuntuBase.sh
+++ ScriptUbuntuBase.sh
@@ -19,4 +19,4 @@ sudo apt update
 sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`" -s ask
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> .zshrc
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
