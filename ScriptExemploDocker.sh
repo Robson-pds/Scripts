94100178d314971efed7a47b3e1b3038b41d07df
Robson Pereira Da Silva <43390496+Robson-pds@users.noreply.github.com>
2024-10-04 08:39:01 -0400

    Adicionado MariaDB ao exemplo

--- ScriptExemploDocker.sh
+++ ScriptExemploDocker.sh
@@ -3,5 +3,7 @@
 docker run --name ServerpostgresSQL -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 #Imagem padrão do Mysql
 docker run --name ServerMysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysecretpassword -d mysql
+#Imagem padrão do MariaDB
+docker run --name ServerMariadb -p 3307:3306 -e MYSQL_ROOT_PASSWORD=mysecretpassword -d mariadb
 #Imagem padrão do Redis
 docker run --name ServerRedis -p 6379:6379 -d redis
