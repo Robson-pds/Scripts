3c7d94712294a562ea594ee9a526d16737069e5f
Robson Pereira Da Silva <43390496+Robson-pds@users.noreply.github.com>
2025-12-17 16:07:13 -0400

    Update Node.js major version to 24

--- ScriptUbuntuNodejs.sh
+++ ScriptUbuntuNodejs.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=22
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y -f nodejs yarn
